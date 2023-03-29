639f4493ea0ca00aec4e59749f90da22b49c76e6
Thành Tấn Trương <64310557+truongtanthanh1202@users.noreply.github.com>
2023-03-30 02:03:59 +0700

    add setting environment

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import detectDifference
 
 if __name__ == '__main__':
     path = './Image/image2.jpg'
+    # 'easy', 'medium', 'hard'
     level = 'medium'
     limit = 10
     createImage.CreateImage(path, level, limit)
     detectDifference.DetectDifference()
-
